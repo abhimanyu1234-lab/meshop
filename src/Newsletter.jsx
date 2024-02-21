ee99c316781ad8abfcb91dabb25f8d6fb3f7edec
abhimanyu1234-lab <123452307+abhimanyu1234-lab@users.noreply.github.com>
2024-02-21 19:41:44 +0530

    Update Newsletter.jsx

--- src/Newsletter.jsx
+++ src/Newsletter.jsx
@@ -1,67 +1,3 @@
-// import {Send} from '@material-ui/icons';
-// import styled from 'styled-components';
-// import React from 'react';
-// import { mobile } from './responsive';
-
-// const Container=styled.div`
-// height: 60vh;
-// background-color: #fcf5f5;
-// color: black; /* Add a specific color for text */
-// display: flex;
-// align-items: center;
-// justify-content: center;
-// flex-direction: column;
-// `
-// const Title=styled.h1`
-// font-size:70px;
-// margin-bottom: 20px;
-
-// `
-// const Desc=styled.div`
-// font-size: 24px;
-// font-weight: 300;
-// margin-bottom: 20px;
-// ${mobile({textAlign:"center"})}
-// `
-// const InputContainer=styled.div`
-// width: 50%;
-// height: 40px;
-// background-color: white;
-// display: flex;
-// justify-content: space-between;
-// border: 1px solid lightgray;
-// ${mobile({width:"80%"})}
-// `
-// const Input=styled.input`
-// border: none;
-// flex: 8;
-// padding-left: 20px;
-// `
-// const Button=styled.button`
-// flex: 1;
-// border: none;
-// background-color: teal;
-// color: white;
-// `
-
-// const Newsletter = () => {
-//   return (
-//     <Container>
-//       <Title>Newsletter</Title>
-//       <Desc>Get Timely Updates From Your Favorite Products.</Desc>
-//       <InputContainer>
-//         <Input placeholder="Your Email"/>
-//         <Button>
-//             <Send/>
-//         </Button>
-//       </InputContainer>
-//     </Container>
-//   )
-// }
-
-// export default Newsletter;
-
-
 import { Send } from '@material-ui/icons';
 import styled from 'styled-components';
 import React, { useState } from 'react';
@@ -86,6 +22,7 @@ border-radius: 10px; /* Add border-radius for rounded corners */
 
 const Title = styled.h1`
   font-size: 70px;
+  ${mobile({ fontSize: "60px" })}
   margin-bottom: 20px;
 `;
 
