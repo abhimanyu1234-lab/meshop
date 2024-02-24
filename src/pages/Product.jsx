09a81e4fafa6a8d3258674574be877cf058b32b4
abhimanyu1234-lab <123452307+abhimanyu1234-lab@users.noreply.github.com>
2024-02-24 19:27:20 +0530

    Update Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -38,7 +38,7 @@ const InfoContainer = styled.div`
 flex: 1;
 padding: 0px 50px;
 ${'' /* margin-top:1px; */}
-${mobile({padding:"5px"})}
+${mobile({padding:"0px 25px"})}
 `
 
 const Title = styled.h1`
@@ -50,20 +50,20 @@ margin: 20px 0px;
 ${'' /* background: linear-gradient(to right, #56ab2f, #a8e063); */}
 color: linear-gradient(to right, #06beb6, #48b1bf);
 
-
+${mobile({margin:"10px 0px"})}
 font-size: 20px;
 `
 
 const Price = styled.span`
-font-weight: 100;
-font-size: 30px;
+font-weight: 200;
+font-size: 25px;
 `
 const FilterContainer=styled.div`
 width:50%;
 margin: 30px 0px;
 display: flex;
 justify-content: space-between;
-${mobile({width:"100%"})}
+${mobile({width:"100%",margin: "10px 0px" })}
 `
 
 const Filter=styled.div`
@@ -229,3 +229,4 @@ const Product = () => {
 }
 
 export default Product;
+
